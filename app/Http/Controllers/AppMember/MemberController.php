291a40c9d1bf2078048ba2f4fa7cf89802630391
hibatulwafi <hiwapiputra@gmail.com>
2025-04-26 15:18:35 +0700

    chore: update member api responses

--- app/Http/Controllers/AppMember/MemberController.php
+++ app/Http/Controllers/AppMember/MemberController.php
@@ -16,9 +16,16 @@ class MemberController extends Controller
         // Anda bisa memilih data profil mana saja yang ingin Anda kembalikan
         return response()->json([
             'id' => $member->id,
+            'photo' => $member->photo,
             'name' => $member->name,
             'email' => $member->email,
-            // Tambahkan informasi profil lain yang relevan
+            'phone' => $member->phone,
+            'total_point' => $member->total_point,
+            'birth_date' => $member->birth_date,
+            'gender' => $member->gender,
+            'address' => $member->address,
+            'is_email_verified' => $member->is_email_verified,
+            'is_phone_verified' => $member->is_phone_verified,
             'created_at' => $member->created_at,
             'updated_at' => $member->updated_at,
         ], 200);
